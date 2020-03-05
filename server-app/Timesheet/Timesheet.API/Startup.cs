c34c4b24f60c045fb291d4806febf1825637aaf4
Kamil Jaworski <kamilj610@gmail.com>
2020-03-05 19:23:05 +0100

    Configure IIS in startup

--- server-app/Timesheet/Timesheet.API/Startup.cs
+++ server-app/Timesheet/Timesheet.API/Startup.cs
@@ -26,6 +26,16 @@ namespace Timesheet.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AutomaticAuthentication = false;
+            });
+
+            services.Configure<IISOptions>(options =>
+            {
+                options.ForwardClientCertificate = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
