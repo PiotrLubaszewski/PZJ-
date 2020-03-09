ee58aa37a6dcd3a99205befd82f257c67cac11a9
Kamil Jaworski <kamilj610@gmail.com>
2020-03-09 21:54:50 +0100

    Add automatic context migrations

--- server-app/Timesheet/Timesheet.API/Program.cs
+++ server-app/Timesheet/Timesheet.API/Program.cs
@@ -1,13 +1,24 @@
 namespace Timesheet.Api
 {
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Timesheet.Persistence;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetService<TimesheetContext>();
+                db.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
