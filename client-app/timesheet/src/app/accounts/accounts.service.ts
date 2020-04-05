c7a888a727a7c49da809f0de5b8e39ef14fa8a9d
Piotr Lubaszewski <piotrlubaszewski@gmail.com>
2020-04-05 23:23:34 +0200

    accounts added

--- client-app/timesheet/src/app/accounts/accounts.service.ts
+++ client-app/timesheet/src/app/accounts/accounts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ApiResponse } from '../models/apiResponse.model';
 import { CollectionResult } from '../models/collectionResult.model';
 import { AccountModel } from '../models/account.model';
@@ -21,31 +21,57 @@ export class AccountsService {
     { pageSize = '', currentPage = '', sort = '' }:
     { pageSize?: string; currentPage?: string; sort?: string; } = {}):
     Observable<ApiResponse<CollectionResult<AccountModel>>> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+      })
+    };
     return this.http.get<ApiResponse<CollectionResult<AccountModel>>>(
       this.origin +
-    '/accounts/?PageSize=' + pageSize +
-    '&CurrentPage=' + currentPage +
-    '&Sort=' + sort);
+    '/accounts/'
+    // + pageSize ? '?pageSize=' + pageSize : ''
+    // + currentPage ? '&currentPage=' + currentPage : ''
+    // + sort ? '&sort=' + sort : ''
+    , httpOptions);
   }
 
   postAccounts(account: AccountModel): Observable<ApiResponse<string>> {
     const requestBody: AccountModel = account;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+      })
+    };
     return this.http.post<ApiResponse<string>>(
       this.origin +
-      '/accounts', requestBody);
+      '/accounts', requestBody, httpOptions);
   }
 
   authorize(user: {userName: string, password: string}): Observable<ApiResponse<string>> {
     const requestBody: {userName: string, password: string} = user;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+      })
+    };
     return this.http.post<ApiResponse<string>>(
       this.origin +
       '/accounts/authorize', requestBody);
   }
 
   getUser(userId: string): Observable<ApiResponse<AccountModel>> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+      })
+    };
     return this.http.get<ApiResponse<AccountModel>>(
       this.origin +
-      '/accounts/' + userId);
+      '/accounts/' + userId, httpOptions);
   }
 
   getUserRoles(
@@ -53,39 +79,63 @@ export class AccountsService {
     { pageSize = '', currentPage = '', sort = '' }:
     { pageSize?: string; currentPage?: string; sort?: string; } = {}):
   Observable<ApiResponse<RoleModel>> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+      })
+    };
     return this.http.get<ApiResponse<RoleModel>>(
       this.origin +
       '/accounts/' + userId +
     '/roles?PageSize=' + pageSize +
     '&CurrentPage=' + currentPage +
-    '&Sort=' + sort) ;
+    '&Sort=' + sort, httpOptions) ;
   }
 
   addUserRoles(userId: string, rolesIds: string[]): Observable<ApiResponse<string>> {
     const requestBody = rolesIds;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+      })
+    };
     return this.http.post<ApiResponse<string>>(
       this.origin +
       '/accounts/' + userId +
-    '/roles', requestBody);
+    '/roles', requestBody, httpOptions);
   }
 
   editUserRoles(userId: string, rolesIds: string[]): Observable<ApiResponse<string>> {
     const requestBody = rolesIds;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+      })
+    };
     return this.http.put<ApiResponse<string>>(
       this.origin +
       '/accounts/' + userId +
-    '/roles', requestBody);
+    '/roles', requestBody, httpOptions);
   }
 
   getRoles(
     { pageSize = '', currentPage = '', sort = '' }:
     { pageSize?: string; currentPage?: string; sort?: string; } = {}):
     Observable<ApiResponse<CollectionResult<RoleModel>>> {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+        })
+      };
       return this.http.get<ApiResponse<CollectionResult<RoleModel>>>(
         this.origin +
         '/roles?PageSize=' + pageSize +
         '&CurrentPage=' + currentPage +
-        '&Sort=' + sort);
+        '&Sort=' + sort, httpOptions);
   }
 
   getUserSalaries(
@@ -93,48 +143,90 @@ export class AccountsService {
     { pageSize = '', currentPage = '', sort = '' }:
     { pageSize?: string; currentPage?: string; sort?: string; } = {}):
     Observable<ApiResponse<CollectionResult<SalaryModel>>>  {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+        })
+      };
       return this.http.get<ApiResponse<CollectionResult<SalaryModel>>>(
         this.origin +
         '/accounts/' + userId +'/salaries?PageSize=' + pageSize +
         '&CurrentPage=' + currentPage +
-        '&Sort=' + sort);
+        '&Sort=' + sort, httpOptions);
   }
 
   addUserSalaries(userId: string, salary: SalaryModel): Observable<ApiResponse<string>> {
     const requestBody = salary;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+      })
+    };
     return this.http.post<ApiResponse<string>>(
       this.origin +
-      '/accounts/' + userId + '/salaries', requestBody );
+      '/accounts/' + userId + '/salaries', requestBody, httpOptions );
   }
 
   editUserSalaries(userId: string, salary: SalaryModel): Observable<ApiResponse<string>> {
     const requestBody = salary;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+      })
+    };
     return this.http.put<ApiResponse<string>>(
       this.origin +
-      '/accounts/' + userId + '/salaries', requestBody );
+      '/accounts/' + userId + '/salaries', requestBody, httpOptions );
   }
 
   getUserSalary(userId: string, salaryId: string): Observable<ApiResponse<SalaryModel>> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+      })
+    };
     return this.http.get<ApiResponse<SalaryModel>>(
       this.origin +
-      '/accounts/' + userId + '/salaries/' + salaryId);
+      '/accounts/' + userId + '/salaries/' + salaryId, httpOptions);
   }
 
   deleteUserSalary(userId: string, salaryId: string): Observable<ApiResponse<string>> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+      })
+    };
     return this.http.delete<ApiResponse<string>>(
       this.origin +
-      '/accounts/' + userId + '/salaries/' + salaryId);
+      '/accounts/' + userId + '/salaries/' + salaryId, httpOptions);
   }
 
   getUserCurrentSalary(userId: string): Observable<ApiResponse<SalaryModel>> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+      })
+    };
     return this.http.get<ApiResponse<SalaryModel>>(
       this.origin +
-      '/accounts/' + userId + '/salaries/current');
+      '/accounts/' + userId + '/salaries/current', httpOptions);
   }
 
   getUserLastSalary(userId: string): Observable<ApiResponse<SalaryModel>> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('user')
+      })
+    };
     return this.http.get<ApiResponse<SalaryModel>>(
       this.origin +
-      '/accounts/' + userId + '/salaries/last');
+      '/accounts/' + userId + '/salaries/last', httpOptions);
   }
 }
--- client-app/timesheet/src/app/app-routing.module.ts
+++ client-app/timesheet/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
- import { LoginComponent } from './core/login/login.component';
+import { LoginComponent } from './core/login/login.component';
 
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'accounts', loadChildren: () => import('./accounts/accounts.module').then(m => m.AccountsModule)}
 ];
 
 @NgModule({
