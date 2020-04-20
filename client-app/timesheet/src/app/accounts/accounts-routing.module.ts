59580053a6406332098f809500b68efa676327f3
Piotr Lubaszewski <piotrlubaszewski@gmail.com>
2020-04-20 15:26:51 +0200

    account

--- client-app/timesheet/src/app/accounts/accounts-routing.module.ts
+++ client-app/timesheet/src/app/accounts/accounts-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountsComponent } from './accounts/accounts.component';
+import { AccountComponent } from './account/account.component';
 
 
 const routes: Routes = [
-  {path: '', component: AccountsComponent}
+  {path: '', component: AccountsComponent},
+  {path: ':id', component: AccountComponent},
+  {path: 'add', component: AccountComponent},
+  {path: ':id/edit', component: AccountComponent},
 ];
 
 @NgModule({
--- client-app/timesheet/src/app/accounts/accounts.module.ts
+++ client-app/timesheet/src/app/accounts/accounts.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AccountsComponent } from './accounts/accounts.component';
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { MaterialModule } from '../shared/material/material.module';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
-  declarations: [AccountsComponent],
+  declarations: [AccountsComponent, AccountComponent],
   imports: [
     CommonModule,
     MaterialModule,
